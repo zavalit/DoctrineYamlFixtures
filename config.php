9957d3b63bea24b5223208e707177bcd3031108b
Vsevolod Dolgopolov <zavalit@gmail.com>
2014-05-01 20:32:31 +0200

    remove config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$dbConfig = array(
-  'driver'=>'pdo_mysql',
-  'host'=>'127.0.0.1',
-  'dbname'=>'doctrine_yaml_fixtures',
-  'user'=>'root',
-  'password'=>'root'
-);
-$yamlMetadataPath = __DIR__."/app/yaml/maml";
